d5ddc02ff54ab6b7149abe97f146fd2c18377b2a
Aabhishek29 <starkabhishek29@gmail.com>
2022-08-03 16:22:33 +0530

    Login update

--- app/src/main/java/com/zeusforth/ano/authentication/LoginActivity.kt
+++ app/src/main/java/com/zeusforth/ano/authentication/LoginActivity.kt
@@ -1,21 +1,27 @@
 package com.zeusforth.ano.authentication
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.zeusforth.ano.HomeActivity
 import com.zeusforth.ano.R
+import org.json.JSONException
 
 
 class LoginActivity : AppCompatActivity() {
+    private val TAG : String  = "LoginActvity";
     private var requestQueue: RequestQueue? = null
     lateinit var username: EditText
     lateinit var password:EditText
@@ -54,39 +60,46 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
-    private fun jsonParse(name: String , pass: String){
-//        val url = "https://staging-webservice.supportgenie.io/v3/agent/validate?email=${name}&&password=${pass}"
-//        val request = JsonObjectRequest(Request.Method.GET, url, null, {
-//                response ->try {
-//            val JsonData = response.getJSONObject("agent")
-//            companyId = JsonData.getString("companyId").toString()
-//            agentId = JsonData.getString("userId").toString()
+    private fun jsonParse(name: String , pass: String) {
+        Log.e(TAG, "${name} and ${pass}")
+        val url =
+            "https://amsportalapp.herokuapp.com/api/users/auth?username=${name}&password=${pass}"
+        try{
+        val request = JsonObjectRequest(Request.Method.GET, url, null, { response ->
+            try {
+                Log.e(TAG, response.toString())
 //            Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show()
-//            val intent = Intent(this,HomeActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-////            val intent = Intent(this,TempHomeActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-//
-//
-//            sharedPreferences = baseContext.getSharedPreferences("MY_PREF", Context.MODE_PRIVATE)
+                val intent = Intent(this, HomeActivity::class.java)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+
+//            sharedPreferences = baseContext.getSharedPreferences("
+//            MY_PREF", Context.MODE_PRIVATE)
 //            val editor = sharedPreferences.edit()
 //            editor.putString("agent_id", agentId)
 //            editor.putString("company_id", companyId)
 //            editor.putString("name", name)
 //            editor.putString("pass", pass)
 //            editor.apply()
-//
-//            progress_bar.visibility = View.INVISIBLE
-//            startActivity(intent)
-//        } catch (e: JSONException) {
-//            e.printStackTrace()
-//        }
-//        }, { error -> error.printStackTrace()
-//            Toast.makeText(this,"Login Failed",Toast.LENGTH_SHORT).show()
-//            username.text.clear()
-//            password.text.clear()
-//            progress_bar.visibility = View.INVISIBLE
-//        })
-//        requestQueue?.add(request)
+
+                progress_bar.visibility = View.INVISIBLE
+                startActivity(intent)
+            } catch (e: JSONException) {
+                Log.e(TAG, e.toString())
+                e.printStackTrace()
+            }
+        }, { error ->
+            error.printStackTrace()
+            Log.e(TAG,error.toString())
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+            username.text.clear()
+            password.text.clear()
+            progress_bar.visibility = View.INVISIBLE
+        })
+            requestQueue?.add(request)
+    }catch (e:Error){
+        Log.e(TAG,e.toString())
+    }
+
         progress_bar.visibility = View.GONE
-        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
